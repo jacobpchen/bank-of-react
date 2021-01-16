3258bf38c897d0f6f334f525d6746781a9960363
Jacob Chen <jacobpchen@gmail.com>
2021-01-16 07:57:26 -0500

    implemented user profile component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import React from "react"
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Home from './components/Home'
-import UserProfile from './UserProfile'
+import UserProfile from './components/UserProfile'
 class App extends React.Component {
   constructor() {
     super()
     this.state = {
-      accountBalance: 14568.27
+      accountBalance: 14568.27,
+      currentUser: {
+        userName: 'bob_loblaw',
+        memberSince: '08/23/99'
+      }
     }
   }
-
-
 
   render() {
 
     const HomeComponent = () =>
       (<Home accountBalance={this.state.accountBalance} />)
 
+    const UserProfileComponent = () => (
+      <UserProfile
+        userName={this.state.currentUser.userName}
+        memberSince={this.state.currentUser.memberSince}
+      />
+    )
+
     return (
       <Router>
         <Switch>
-          <Route exact path='/' component={HomeComponent} />
+          <Route exact path='/' render={HomeComponent} />
+          <Route path='/userProfile' render={UserProfileComponent} />
         </Switch>
       </Router>
     )
